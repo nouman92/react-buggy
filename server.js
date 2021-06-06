a85c9a017a4311346430558291827e4b713be799
Sadia Ashfaq <saadiaashfaq2812@gmail.com>
2021-06-06 17:48:14 +0500

    modified code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-const app = express();
+// const app = express();
 const port = process.env.PORT || 3000;
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
@@ -15,6 +15,6 @@ new WebpackDevServer(webpack(config), {
   console.log('Listening at 0.0.0.0');
 });
 
-app.listen(3000, () => {
-  console.log('Hello world listening on port', port);
-});
+// app.listen(3000, () => {
+//   console.log('Hello world listening on port', port);
+// });
