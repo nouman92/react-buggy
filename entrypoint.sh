8da9963d9813593ddbbc01555995cd5d502e0b0e
Sadia Ashfaq <saadiaashfaq2812@gmail.com>
2021-06-24 03:32:22 +0500

    commented entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e # Abort script at first error
 
 args="--regex --entropy=False" # Default trufflehog options
 
-if [ -n "${INPUT_SCANARGUMENTS}" ]; then
-  args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
-fi
+# if [ -n "${INPUT_SCANARGUMENTS}" ]; then
+#   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
+# fi
 
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
 # which is mounted into our image. This means, as long as a checkout action was
