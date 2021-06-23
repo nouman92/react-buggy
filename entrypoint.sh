a45e869aac180d4e7cd5b29a4012619a7e3e4381
Sadia Ashfaq <saadiaashfaq2812@gmail.com>
2021-06-24 03:14:52 +0500

    uncommented entrypoint and changed githubRepo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e # Abort script at first error
 
-args="--regex --entropy=False --max_depth=50" # Default trufflehog options
+args="--regex --entropy=False" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
@@ -11,6 +11,7 @@ fi
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
 # which is mounted into our image. This means, as long as a checkout action was
 # done before our action runs, we'll have access to the repository.
-githubRepo="file://$(pwd)" # Default target repository
+# githubRepo="file://$(pwd)" # Default target repository
+githubRepo="https://github.com/SadiaAshfaq2812/react-buggy.git"
 query="$args $githubRepo" # Build args query with repository url
 trufflehog $query
